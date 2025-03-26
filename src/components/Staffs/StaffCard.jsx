d41909bbca907aa0fb6944bd52b50a7912e50392
jsami31 <57608969+Sadab-7878@users.noreply.github.com>
2025-03-26 14:58:47 +0600

    staff 40% done

---
+++ src/components/Staffs/StaffCard.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { HiMiniArrowRightEndOnRectangle } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+
+
+const StaffCard = ({ staff }) => {
+
+    const navigate = useNavigate()
+  return (
+    <>
+      <div className="bg-gray-50 border hover:border-sky-800 rounded-md shadow-md p-5">
+        <div className="md:flex items-center">
+          <div>
+            <img
+              src={staff.image}
+              alt={staff.name}
+              className="w-24 rounded-full mx-auto"
+            />
+          </div>
+
+          <div className="md:pl-5 mt-5 md:m-0">
+            <h2 className="text-xl font-bold">{staff.name}</h2>
+            <h3 className="text-sm font-bold">{staff.bio}</h3>
+            <h3 className="text-xs text-gray-600">{staff.location}</h3>
+            <h4 className="text-sm text-gray-800">${staff.rate}</h4>
+          </div>
+        </div>
+
+        <p className="line-clamp-2 mt-5 text-justify text-sm text-gray-600">
+          {staff.details}
+        </p>
+
+        <div className="flex flex-wrap gap-2 my-2 text-xs items-center h-32 lg:h-20">
+          {staff.services.length > 3 ? (
+            <>
+              {staff.services.slice(0, 3).map((service) => (
+                <p className="border rounded-full mr-2 p-2 border-gray-700">
+                  {service}
+                </p>
+              ))}
+
+              <p className="pl-2 ">and {staff.services.length - 3} more...</p>
+            </>
+          ) : (
+            staff.services.map((service) => (
+              <p className="border rounded-full mr-2 p-2 border-gray-700">
+                {service}
+              </p>
+            ))
+          )}
+        </div>
+
+        <button onClick={()=> {
+            navigate(`/staff-details/${staff.id}`)
+        }} className="bg-sky-800 hover:bg-sky-900 text-white rounded-full text-sm  mt-2 flex items-center py-2 px-5 gap-1">
+          See Profile <HiMiniArrowRightEndOnRectangle />
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default StaffCard;
--- src/pages/Staffs/Staffs.jsx
+++ src/pages/Staffs/Staffs.jsx
@@ -1,14 +1,56 @@
-import React from 'react';
-import Navbar from '../../components/Navbar';
+import React from "react";
+import Navbar from "../../components/Navbar";
+import StaffCard from "../../components/Staffs/StaffCard";
 
 const Staffs = () => {
-    return (
-        <div>
+  const staffs = [
+    {
+      id: 1,
+      name: "John Doe",
+      bio: "Experienced technician specializing in home appliance repairs and maintenance.",
+      location: "New York, USA",
+      rate: 4.8,
+      details:
+        "John has over 10 years of experience in repairing home appliances, including ACs, refrigerators, and washing machines.",
+      image: "https://i.ibb.co.com/fdFVMtws/10496279.jpg",
+      services: [
+        "AC Repair Services",
+        "Home Appliance Repair",
+        "Electrician Services",
+        "Plumbing Services",
+      ],
+    },
+    {
+      id: 2,
+      name: "Emily Smith",
+      bio: "Professional beauty and wellness expert with a passion for self-care and relaxation.",
+      location: "Los Angeles, USA",
+      rate: 4.9,
+      details:
+        "Emily has been in the beauty and wellness industry for 8 years, offering top-notch spa and personal care services.",
+      image: "https://i.ibb.co.com/XZN6nH4W/11475221.jpg",
+      services: [
+        "Beauty & Wellness",
+        "Fitness & Personal Training",
+        "Cleaning Solution",
+        "Home Painting Services",
+      ],
+    },
+  ];
 
-            <Navbar />
-            
+  return (
+    <div>
+      <Navbar />
+
+      <div className="container mx-auto p-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+          {staffs.map((staff) => (
+            <StaffCard key={staff.id} staff={staff} />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Staffs;
